90ee7a579f2b50baf374176844646c70ac1350bc
Pierre Joye <pierre.php@gmail.com>
2015-05-17 07:22:40 +0700

    intitial import

---
+++ src/Application.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace Pool;
+
+use Pool\Entity\User;
+use Pool\Entity\UserRepository;
+use RKA\Slim;
+
+/**
+ * Class Application.
+ */
+class Application extends Slim
+{
+    /**
+     * @var callable
+     */
+    private $authentication;
+
+    /**
+     * @var array
+     */
+    private $innerCache = [];
+
+    /**
+     * @param array $userSettings
+     */
+    public function __construct(array $userSettings = array())
+    {
+        parent::__construct($userSettings);
+
+        session_start();
+
+        $this->authentication = function () {
+            if ($this->user() === null) {
+                $this->redirect('/login');
+            }
+        };
+    }
+
+    /**
+     * @return User|null
+     */
+    public function user()
+    {
+        if (!isset($_SESSION['user'])) {
+            return;
+        };
+
+        if (!isset($this->innerCache['user'])) {
+            /* @var $userRepository UserRepository */
+            $userRepository = $this->container->get('user.repository');
+            $user = $userRepository->find($_SESSION['user']);
+
+            if (is_null($user)) {
+                return;
+            }
+
+            $this->innerCache['user'] = $user;
+        }
+
+        return $this->innerCache['user'];
+    }
+
+    /**
+     * @param mixed    $condition
+     * @param string   $url
+     * @param int|null $status
+     *
+     * @return $this
+     */
+    public function redirectIf($condition, $url, $status = null)
+    {
+        if ((bool) $condition) {
+            $this->redirect($url, $status ?: 302);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param mixed    $condition
+     * @param string   $url
+     * @param int|null $status
+     *
+     * @return $this
+     */
+    public function redirectUnless($condition, $url, $status = null)
+    {
+        if ((bool) $condition === false) {
+            $this->redirect($url, $status ?: 302);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $condition
+     *
+     * @return $this
+     */
+    public function notFoundIf($condition)
+    {
+        if ((bool) $condition === true) {
+            $this->notFound();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param int $code
+     *
+     * @return $this
+     */
+    public function renderError($code)
+    {
+        $this->render('errors/'.$code.'.html');
+        $this->response()->status($code);
+        $this->stop();
+
+        return $this;
+    }
+
+    /**
+     * @param string $template
+     * @param array  $data
+     * @param null   $status
+     */
+    public function render($template, $data = array(), $status = null)
+    {
+        // add user in template data
+        $data = array_merge(
+            [
+                'user' => $this->user(),
+            ],
+            $data ?: []
+        );
+
+        parent::render($template, $data, $status);
+    }
+
+    /**
+     * @param callable $callback
+     *
+     * @return $this
+     */
+    public function then(callable $callback)
+    {
+        $callback($this);
+
+        return $this;
+    }
+
+    /**
+     * @param callable $callback
+     *
+     * @return $this
+     */
+    public function otherwise(callable $callback)
+    {
+        return $this->then($callback);
+    }
+
+    /**
+     * Run application.
+     */
+    public function run()
+    {
+        $this->error(
+            function () {
+                $this->renderError(500);
+            }
+        );
+
+        $this->notFound(
+            function () {
+                $this->renderError(404);
+            }
+        );
+
+        parent::run();
+    }
+
+    /**
+     * @param string          $route
+     * @param callable|string $callable
+     *
+     * @return \Slim\Route
+     */
+    public function getSecured($route, $callable)
+    {
+        return $this->get($route, $this->authentication, $callable);
+    }
+
+    /**
+     * @param string          $route
+     * @param callable|string $callable
+     *
+     * @return \Slim\Route
+     */
+    public function postSecured($route, $callable)
+    {
+        return $this->post($route, $this->authentication, $callable);
+    }
+
+    /**
+     * @param string $body
+     * @param int    $code
+     *
+     * @return \Slim\Route
+     */
+    public function jsonResponse($body, $code)
+    {
+        $response = $this->response();
+        $response['Content-Type'] = 'application/json';
+        $response->status($code);
+        $response->body(json_encode($body));
+
+        return $this;
+    }
+}
+
+/* vim: set tabstop=4 shiftwidth=4 expandtab: fdm=marker */
---
+++ web/index.php
@@ -0,0 +1,36 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+
+function check_or_create_json_dir(\Pool\Application $app)
+{
+    if (is_dir($app->config('json_path')) === false) {
+        mkdir($app->config('json_path'), 0777, true);
+        mkdir($app->config('json_path').'users/github', 0777, true);
+        mkdir($app->config('json_path').'extensions', 0777, true);
+    }
+}
+
+function check_or_create_json_dir(\Pool\Application $app)
+{
+    if (is_dir($app->config('json_path')) === false) {
+        mkdir($app->config('json_path'), 0777, true);
+        mkdir($app->config('json_path').'users/github', 0777, true);
+        mkdir($app->config('json_path').'extensions', 0777, true);
+    }
+}
+
+$cacheDir = __DIR__.'/../cache-dir/';
+$app = new \Pool\Application(
+    [
+		'view'      => new \Pool\View\Twig(),
+        //'view'      => new \Pool\View\Twig(['cache' => $cacheDir . 'twig']),
+        'json_path' => __DIR__.'/json/',
+        'cache_dir' => $cacheDir,
+        'web_root_dir' => __DIR__,
+    ]
+);
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+$app->run();
