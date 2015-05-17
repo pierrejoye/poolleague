03cf672dfab05cf9aa0a3dc98ef3b4dc75606f17
Pierre Joye <pierre.php@gmail.com>
2015-05-17 12:20:23 +0700

    add docker

--- web/index.php
+++ web/index.php
@@ -1,7 +1,9 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 
-function check_or_create_json_dir(\Pool\Application $app)
+use Pool\Application;
+
+function check_or_create_json_dir(Application $app)
 {
     if (is_dir($app->config('json_path')) === false) {
         mkdir($app->config('json_path'), 0777, true);
@@ -10,16 +12,7 @@ function check_or_create_json_dir(\Pool\Application $app)
     }
 }
 
-function check_or_create_json_dir(\Pool\Application $app)
-{
-    if (is_dir($app->config('json_path')) === false) {
-        mkdir($app->config('json_path'), 0777, true);
-        mkdir($app->config('json_path').'users/github', 0777, true);
-        mkdir($app->config('json_path').'extensions', 0777, true);
-    }
-}
-
-$cacheDir = __DIR__.'/../cache-dir/';
+$cacheDir = __DIR__.'/../cache/';
 $app = new \Pool\Application(
     [
 		'view'      => new \Pool\View\Twig(),
@@ -30,7 +23,35 @@ $app = new \Pool\Application(
     ]
 );
 
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, " . $name;
-});
+// Redis client
+$app->container->singleton(
+    'redis.client',
+    function (Set $container) {
+        $config = $container->get('app.config');
+
+        $client = new Predis\Client(sprintf('tcp://%s:%s', $config['redis']['host'], $config['redis']['port']));
+        $client->select($config['redis']['db']);
+
+        return $client;
+    }
+);
+
+// User repository
+$app->container->singleton(
+    'user.repository',
+    function (Set $container) {
+        return new UserRepository($container->get('redis.client'));
+    }
+);
+
+
+// Default
+$app->get('/', 'Pool\Controller\DefaultController:indexAction');
+
+// Authorization
+$app->get('/login', 'Pool\Controller\AuthController:loginFormAction');
+$app->post('/login', 'Pool\Controller\AuthController:loginAction');
+$app->get('/logout', 'Pool\Controller\AuthController:logoutAction');
+
+
 $app->run();
