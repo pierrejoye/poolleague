982d1b4220dee3aacb1e297cd352fb332fc00350
Pierre Joye <pierre.php@gmail.com>
2015-05-18 08:39:51 +0700

    form hash in abstract controller

--- src/Controller/ControllerAbstract.php
+++ src/Controller/ControllerAbstract.php
@@ -21,6 +21,32 @@ abstract class ControllerAbstract
     {
         $this->app = $app;
     }
+
+    protected function getHash()
+    {
+        $h = bin2hex(openssl_random_pseudo_bytes(16));
+        $_SESSION['h'] = $h;
+        return $h;
+    }
+
+    protected function checkHash()
+    {
+		$hRequest = $this->app->request()->post('h');
+		$hSession = isset($_SESSION['h']) ?  $_SESSION['h'] : false;
+		if (!is_string($hRequest) || !is_string($hSession)) {
+			$failed = true;
+		} else {
+			if (!hash_equals($hSession, $hRequest)) {
+				$failed = true;
+			}
+		}
+
+		unset($_SESSION['h']);
+
+		if ($failed) {
+			$this->app->redirect('/');
+		}
+    }
 }
 
 /* vim: set tabstop=4 shiftwidth=4 expandtab: fdm=marker */
